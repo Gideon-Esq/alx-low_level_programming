2f1c0b8a80cc52383c719d8ee30a6262785ed688
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2023-10-13 01:06:29 -0700

    update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for 0 through 9
+ * @c: int type number
+ * Return: 1 on success and 0 on failure
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+return (1);
+else
+
+return (0);
+}
