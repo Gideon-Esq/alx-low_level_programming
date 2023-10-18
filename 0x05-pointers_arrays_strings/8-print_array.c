7108abebf323614ffdaf66dcc33f4ee659ed08ff
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2023-10-17 19:52:24 -0700

    Fix error

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,9 @@
 /**
  * print_array - print 'n' elements of an array of integers
  * @a: int type array pointer
- * @n: int type integer
+ * @n: int type intege
+ * Description: numbermust be seperated by ', ' snd spacse.
+ * Numbershould be displayed in the case order they are srored in arrays
  */
 
 void print_array(int *a, int n)
