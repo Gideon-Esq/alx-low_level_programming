3ae761fbb798e581578e2188e2461bc6f459aa2a
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2023-10-25 09:39:23 -0700

    Returns the factorial of 1,5,10,-1024.

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - Multiples of a certain number
+ * @n: integer type number
+ * Return: n factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n < 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
+
+
