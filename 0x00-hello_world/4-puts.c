6becc640c52617a7dd6e0f6bf8a5303bd9dfce43
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2023-11-11 04:40:47 -0800

    A C program that print exactly programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints a given phrase
+ * Return: 0 if exited properly, non-zero othwrwise
+ */
+
+int main(void)
+{
+	 char str[51];
+
+	 strcpy(str, "programming is like building a multilingual puzzle");
+	 puts(str);
+
+return (0);
+}
+
