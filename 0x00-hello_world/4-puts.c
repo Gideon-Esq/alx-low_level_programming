6c4bcf21ffe4da17cffa206dc034be71b8ab07de
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2023-11-11 04:51:55 -0800

    A C program that print exactly programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	 char str[52];
 
-	 strcpy(str, "\"programming is like building a multilingual puzzle");
+	 strcpy(str, "\"Programming is like building a multilingual puzzle");
 	 puts(str);
 
 return (0);
