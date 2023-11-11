537ce7e29de0c48715efea70a21b691035efc3b5
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2023-11-11 12:17:03 -0800

    Prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * printf : print out value
+ * Return: Always return 0 (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+		return (0);
+}
+
