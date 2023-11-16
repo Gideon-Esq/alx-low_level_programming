70d954b4046ddf64557b94857472bb374cb4bc46
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2023-11-16 04:01:00 -0800

    prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - The function that print alphabet
+ * Return: Always return 0.
+ */
+
 int main(void)
 {
 	char alphabet = 'a';
 
-	for (alphabet = 'a'; alphabet<= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
 	putchar('\n');
 
