3d4dae0350eb5664dacfcec8364018003313bb4a
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-01-01 14:38:33 -0800

    Prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- display alphabet a-z ten times.
+ * @i,ch: parameters
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	char ch = 'a';
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch < 'z'; ch++)
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+	}
+
+}
+
