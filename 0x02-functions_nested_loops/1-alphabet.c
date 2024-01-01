fe579f499226962511a4195c53322772a4f3be21
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-01-01 11:40:43 -0800

    Prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet - print a-z in small letters.
- * @alp: Parameter for the function 'print_alphabet
- * Return: Alway return 0 (if sucess)
+ * @alp: Parameters)
  */
 
 void print_alphabet(void)
