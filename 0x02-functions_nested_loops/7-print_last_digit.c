6a126aa287e10a9df82a75f31b2a2c9d7a88951d
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-01-02 14:19:49 -0800

    Prints the last digit of a number(hot-fix).

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * 
+ * print_last_digit - prints the last digit of a number.
+ *@x: The number to extract the last digit from.
  *
- *
+ * Return: The last digit of a number.
  */
 
 int print_last_digit(int x)
