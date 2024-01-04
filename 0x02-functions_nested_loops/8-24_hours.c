5e37839efe3a1e39b8dcd9e3bb6513aded9de125
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-01-04 12:16:25 -0800

    Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,40 @@
 #include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ *
+ */
 
 void jack_bauer(void)
 {
-	char j = 0;
-	char a = 0;
-	char c = 0;
-	char k = 0;
+	int j = 0;
+	int a = 0;
+	int c = 0;
+	int k = 0;
 
 	for (j = 0; j <= 2; j++)
-	for (a = 0; a <= 3; a++)
-	for (c = 0; c <= 5; c++)
-	for (k = 0; k <= 9; k++)
 	{
-		_putchar('0' + j);
-		_putchar('0' + a);
-		_putchar(':');
-		_putchar('0' + c);
-		_putchar('0' + k);
-		_putchar('\n');
+		for (a = 0; a <= 9; a++)
+		{
+			if ((j <= 1 && a <= 9) || (j <= 2 && a <= 3))
+			{
+				for (c = 0; c <= 5; c++)
+				{
+					for (k = 0; k <= 9; k++)
+					{
+						{
+							_putchar('0' + j);
+							_putchar('0' + a);
+							_putchar(':');
+							_putchar('0' + c);
+							_putchar('0' + k);
+							_putchar('\n');
+						}
+					}
+				}
+			}
+		}
 	}
-	_putchar('\n');
+}
 
-}	
 
