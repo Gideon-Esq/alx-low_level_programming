e23be5c77c4c487447cf6c0ecdee652ecaea9964
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-01-13 23:45:58 -0800

    Multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,14 @@
 
 int main(int argc, char *argv[])
 {
-	int count, mul;
 
 	if (argc >= 3)
 	{
-		for (count = 1; count < argc; count++)
 		{
 			int count1 = atoi(argv[1]);
 			int count2 = atoi(argv[2]);
-			mul = count1 * count2;
-			/**if (count != 0)*/
 			{ 
-				printf("%d\n", mul);
+				printf("%d\n",count1 * count2);
 			}
 		}
 	}
