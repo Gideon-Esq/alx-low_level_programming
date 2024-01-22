d00cfc044033631cb7ad44127826237b11032348
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-01-22 10:35:33 -0800

    Prints the name of the file it was compiled from, followed by a new line.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,14 @@
-#ifndef __FILE__
-#define __FILE__
-#endif /**__FILE__*/
+#ifndef MY_FILE_H
+#define MY_FILE_H
+
 #include <stdio.h>
+
+#define PRINT_COMPILED_FILE __FILE__
+#endif /** MY_FILE_H*/
 
 int main(void)
 {
-	printf("%s\n", __FILE__);
-
+	printf("%s\n", PRINT_COMPILED_FILE);
 	return (0);
 }
 
