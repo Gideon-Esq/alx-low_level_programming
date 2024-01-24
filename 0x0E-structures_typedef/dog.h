0bacdf7eea1f6c2e25377f0d6b411ce9f923c74e
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-01-23 17:10:15 -0800

    A new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,15 @@
 #define DOG_H
 /**
  * struct dog - A typedef describing a dog
-typedef struct my_dog dog;
+ * @name: Struct first member
+ * @age: Struct second member
+ * @owner: Struct third member
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif /**DOG_H*/
 
