3ce876db5a4b62d93e5587aa807d79598eab9627
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-01-26 08:14:50 -0800

    Prints numbers, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,22 +5,22 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	int i;
+	/*int number;*/
+	unsigned int i, number;
 
+	 va_start(ap, n);
 
-	/**separator =*/
-	
-	va_start(ap, n);
-	
-	for (i = 0; i >= va_arg(ap,int);  i = va_arg(ap, int))
-	{
-		if (separator == NULL)
-		{
-			putchar('"');
-		}
-		printf("%d, ", i/*, separator*/);
-	}
-	va_end(ap);
-	putchar('\n');
+	 for (i = 0; i < n; i++)
+	 {
+		 number = va_arg(ap, int);
+		 printf("%d", number);
+		 if (separator != NULL && i != number - 1)
+		 {
+			 printf("%s", separator);
+		 }
+	 }
+	 va_end(ap);
+	 printf("\n");
 }
+
 
