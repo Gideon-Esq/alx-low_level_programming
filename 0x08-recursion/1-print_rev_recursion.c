aad7275e203d827bb0884c0af241360345647624
Gideon-Esq <138716971+Gideon-Esq@users.noreply.github.com>
2024-02-04 00:58:49 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - print a string in reverse
- * @s: string
+ * _print_rev_recursion - print a string in reverse order.
+ * @s: string parameters.
  */
 
 void _print_rev_recursion(char *s)
