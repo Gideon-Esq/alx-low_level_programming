f2f7752a6543d105d5f283648d4a677ec40b0a20
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-02-09 17:00:13 -0800

    Prints the binary representation of a number.

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c: The result to be printed.
+ *
+ * Return: 1 if success else -1.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
