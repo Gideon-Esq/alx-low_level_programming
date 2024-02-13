b0e9515bf2832f9c1fcdee634119fe72f07ae7bd
Gideon-Esq <138716971+Gideon-Esq@users.noreply.github.com>
2024-02-13 01:35:54 +0100

    Update _putchar.c

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -3,7 +3,7 @@
  * _putchar - write the character c to stdout
  * @c: The result to be printed.
  *
- * Return: 1 if success else -1.
+ * Return: 1 if success else return -1.
  */
 
 int _putchar(char c)
