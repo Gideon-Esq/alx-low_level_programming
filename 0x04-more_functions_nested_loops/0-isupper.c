2a7b07da9e121860cbb23641aebe654282a87e24
Gideon-Esq <138716971+Gideon-Esq@users.noreply.github.com>
2024-02-22 00:57:39 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - function that checks for uppercase characters
  * @c: int type number
- * Return: 1 if uppercase, else 0
+ * Return: 1 if uppercase, else return 0
  */
 int _isupper(int c)
 {
